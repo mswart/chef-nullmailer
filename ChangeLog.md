8f33e59087899c2119e56b28433a393f042ae11e
Malte Swart <chef@malteswart.de>
2013-09-04 23:52:04 +0200

    Release v0.1.1 (improved ReadMe and secured remotes file)

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,10 @@
+0.1.1 / 2013-09-04
+==================
+
+  * Secure remotes file because it may contain passwords (same behavior like
+    the debian package)
+
+
 0.1.0 / 2013-08-16 
 ==================
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'chef@malteswart.de'
 license           'Apache 2.0'
 description       'Installation and configuration of nullmailer'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.1.0'
+version           '0.1.1'
 
 %w{ ubuntu }.each do |os|
   supports os
